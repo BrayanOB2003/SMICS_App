3a19126db10b1e4ca0959764dbd43d3e1276fc66
Brayan Ortiz <brayanortizbastidas82@gmail.com>
2023-01-08 04:56:12 -0500

    Added registration manager and crossing activities.

--- app/src/main/java/com/project/smics_project/FirstFragment.kt
+++ app/src/main/java/com/project/smics_project/FirstFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.content.Intent
+import android.widget.Button
+import com.project.smics_project.R.id.button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +37,13 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+
+        val view: View = inflater.inflate(R.layout.fragment_first, container, false)
+
+        val button = view.findViewById<Button>(R.id.button)
+        button.setOnClickListener { input -> openRegisterManager(input)}
+
+        return view
     }
 
     companion object {
@@ -55,5 +64,10 @@ class FirstFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    fun openRegisterManager(view: View){
+        val newActivity = Intent(activity,QuestionaryActivity::class.java)
+        startActivity(newActivity)
     }
 }
---
+++ app/src/main/java/com/project/smics_project/QuestionaryActivity.kt
@@ -0,0 +1,11 @@
+package com.project.smics_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestionaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_questionary)
+    }
+}
